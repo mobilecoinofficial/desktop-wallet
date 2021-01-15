21d502a14519b27557a79fd917ca7d593397fdf1
David Y. Hall <me@davidyhall.com>
2021-01-14 22:30:40 -0800

    Clean up ImportantAcountView and add tests

---
+++ test/views/auth/ImportAccountView/ImportAccountView.test.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import type { MobileCoinDContextValue } from '../../../../app/contexts/MobileCoinDContext';
+import { ImportAccountView } from '../../../../app/views/auth';
+import renderSnapshot from '../../../renderSnapshot';
+
+jest.mock('../../../../app/hooks/useMobileCoinD');
+
+function setupComponent(contextOverides?: MobileCoinDContextValue) {
+  // Default Context
+  const defaultContext = {
+    encryptedEntropy: 'existing encrypted entropy',
+    isAuthenticated: false,
+  };
+
+  const { asFragment } = renderSnapshot(
+    <ImportAccountView isTest />,
+    {
+      ...defaultContext,
+      ...contextOverides,
+    },
+  );
+
+  // Render Elements
+  const createButton = screen.getByRole('button', {
+    name: 'Create account instead',
+  });
+  const overwriteWarningQuery = screen.queryAllByTestId('overwrite-warning');
+
+  return {
+    asFragment,
+    createButton,
+    overwriteWarningQuery,
+  };
+}
+
+describe('ImportAccountView', () => {
+  describe('component', () => {
+    test('it navigates to ImportAccountView with button click', () => {
+      const { createButton } = setupComponent();
+
+      expect(createButton).toBeInTheDocument();
+
+      userEvent.click(createButton);
+
+      expect(createButton).not.toBeInTheDocument();
+      expect(
+        screen.getByTestId('CreateAccountView'),
+      ).toBeInTheDocument();
+    });
+
+    describe('unlock warning', () => {
+      test('when there is an existing user, it renders the warning', () => {
+        const { overwriteWarningQuery } = setupComponent();
+
+        expect(overwriteWarningQuery.length).toBe(1);
+      });
+
+      test('warning allow you to navigate to unlock wallet', () => {
+        setupComponent();
+        const unlockButton = screen.getByRole('button', {
+          name: 'Unlock this wallet',
+        });
+
+        userEvent.click(unlockButton);
+
+        expect(unlockButton).not.toBeInTheDocument();
+        expect(
+          screen.getByTestId('UnlockWalletView'),
+        ).toBeInTheDocument();
+      });
+
+      test('it does not render warning without existing user', () => {
+        // @ts-ignore mock
+        const { overwriteWarningQuery } = setupComponent({ encryptedEntropy: undefined });
+
+        expect(overwriteWarningQuery.length).toBe(0);
+      });
+    });
+
+    describe('render', () => {
+      test('it renders correctly', () => {
+        const { asFragment } = setupComponent();
+        expect(asFragment()).toMatchSnapshot();
+      });
+    });
+  });
+});

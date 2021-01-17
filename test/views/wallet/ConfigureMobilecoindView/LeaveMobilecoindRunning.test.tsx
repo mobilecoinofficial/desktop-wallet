1201aeee3db803fd969fb58b560f44d4c0bb097a
johnedma <johned.matt@gmail.com>
2021-01-17 13:40:56 -0600

    test filename typo corrected and update of LeaveMobilecoindRunning for more exhaustive view testing

--- test/views/wallet/ConfigureMobilecoindView/LeaveMobilecoindRunning.test.tsx
+++ test/views/wallet/ConfigureMobilecoindView/LeaveMobilecoindRunning.test.tsx
@@ -1,64 +1,86 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
 import React from 'react';
 
-import { fireEvent, render, screen } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import useMobilecoindConfigs from '../../../../app/hooks/useMobilecoindConfigs';
 import LeaveMobilecoindRunning from '../../../../app/views/wallet/ConfigureMobilecoindView';
+import renderSnapshot from '../../../renderSnapshot';
 
 jest.mock('../../../../app/hooks/useMobilecoindConfigs');
-
-function setupComponent() {
-  const mockLeaveMobilecoindRunning = jest.fn();
+function setupComponent(isLeaveMobilecoindRunning: boolean) {
   const mockToggleLeaveMobilecoindRunning = jest.fn();
   const mockUseMobilecoindConfigs = useMobilecoindConfigs as jest.MockedFunction<
     typeof useMobilecoindConfigs
   >;
-
   // @ts-ignore mock
   mockUseMobilecoindConfigs.mockImplementation(() => {
     return {
-      leaveMobilecoindRunning: mockLeaveMobilecoindRunning,
+      leaveMobilecoindRunning: isLeaveMobilecoindRunning,
       toggleLeaveMobilecoindRunning: mockToggleLeaveMobilecoindRunning,
     };
   });
+  renderSnapshot(<LeaveMobilecoindRunning />);
+  // Render Elements
+  const offQuery = screen.queryByText('Leave MobileCoinD Active is off');
+  const onQuery = screen.queryByText('Leave MobileCoinD Active is on');
+  const offDescriptionQuery = screen.queryByText(
+    'MobileCoinD will not continue to sync when you exit the wallet.'
+  );
+  const onDescriptionQuery = screen.queryByText(
+    'MobileCoinD will continue to sync when you exit the wallet.'
+  );
+  const toggleSwitch = screen.getByRole('checkbox') as HTMLInputElement;
   return {
-    mockLeaveMobilecoindRunning,
     mockToggleLeaveMobilecoindRunning,
+    offDescriptionQuery,
+    offQuery,
+    onDescriptionQuery,
+    onQuery,
+    toggleSwitch,
   };
 }
-
 describe('LeaveMobilecoindRunning', () => {
-  test('Leave Mobilecoind Running View renders', () => {
-    setupComponent();
-    const history = createMemoryHistory();
-    render(
-      <Router history={history}>
-        <LeaveMobilecoindRunning />
-      </Router>
-    );
-
-    expect(
-      screen.getByText(/MobileCoinD Background Process/i)
-    ).toBeInTheDocument();
+  describe('when leaveMobilecoindRunning is false', () => {
+    test('the CTA is to toggle it on', () => {
+      const { offQuery, onQuery } = setupComponent(false);
+      expect(offQuery).toBeInTheDocument();
+      expect(onQuery).not.toBeInTheDocument();
+    });
+    test('the description is correct', () => {
+      const { offDescriptionQuery, onDescriptionQuery } = setupComponent(false);
+      expect(offDescriptionQuery).toBeInTheDocument();
+      expect(onDescriptionQuery).not.toBeInTheDocument();
+    });
+    test('the switch is off', () => {
+      const { toggleSwitch } = setupComponent(false);
+      expect(toggleSwitch.checked).toBe(false);
+    });
   });
-
-  test('toggleLeaveMobilecoindRunning should change state', () => {
-    const { mockLeaveMobilecoindRunning } = setupComponent();
-    const history = createMemoryHistory();
-    render(
-      <Router history={history}>
-        <LeaveMobilecoindRunning />
-      </Router>
-    );
-    expect(mockLeaveMobilecoindRunning).toBeFalsy;
-    expect(screen.queryByText('Leave MobileCoinD Active is on'))
-      .toBeInTheDocument;
-
-    fireEvent.click(screen.getByRole('checkbox'));
-    expect(screen.queryByText('Leave MobileCoinD Active is off'))
-      .toBeInTheDocument;
+  describe('when leaveMobilecoindRunning is true', () => {
+    test('the CTA is to toggle it on', () => {
+      const { offQuery, onQuery } = setupComponent(true);
+      expect(offQuery).not.toBeInTheDocument();
+      expect(onQuery).toBeInTheDocument();
+    });
+    test('the description is correct', () => {
+      const { offDescriptionQuery, onDescriptionQuery } = setupComponent(true);
+      expect(offDescriptionQuery).not.toBeInTheDocument();
+      expect(onDescriptionQuery).toBeInTheDocument();
+    });
+    test('the switch is off', () => {
+      const { toggleSwitch } = setupComponent(true);
+      expect(toggleSwitch.checked).toBe(true);
+    });
+  });
+  describe('toggle switch', () => {
+    test('it calls toggleLeaveMobilecoindRunning on click', () => {
+      const {
+        mockToggleLeaveMobilecoindRunning,
+        toggleSwitch,
+      } = setupComponent(true);
+      userEvent.click(toggleSwitch);
+      expect(mockToggleLeaveMobilecoindRunning).toBeCalled();
+    });
   });
 });

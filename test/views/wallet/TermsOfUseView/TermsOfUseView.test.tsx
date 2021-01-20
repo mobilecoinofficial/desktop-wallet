0e31f001ec26cbfc49fb71510924fd657e3023dc
johnedma <johned.matt@gmail.com>
2021-01-20 14:22:11 -0600

    added test for breadcrumb navigation

--- test/views/wallet/TermsOfUseView/TermsOfUseView.test.tsx
+++ test/views/wallet/TermsOfUseView/TermsOfUseView.test.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { TermsOfUseView } from '../../../../app/views/wallet';
 import renderSnapshot from '../../../renderSnapshot';
 
@@ -9,6 +12,16 @@ describe('TermsOfUseView', () => {
       test('it renders correctly', () => {
         const { asFragment } = renderSnapshot(<TermsOfUseView />);
         expect(asFragment()).toMatchSnapshot();
+      });
+    });
+
+    describe('breadcrumb navigation', () => {
+      test('Settings breadcrumb navigates away from current view', () => {
+        renderSnapshot(<TermsOfUseView />);
+        const termsPanel = screen.queryByText('MOBILECOIN TERMS OF USE FOR MOBILECOINS AND MOBILECOIN WALLETS');
+        expect(termsPanel).toBeInTheDocument();
+        userEvent.click(screen.getByText('Settings'));
+        expect(termsPanel).not.toBeInTheDocument();
       });
     });
   });

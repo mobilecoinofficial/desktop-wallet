f310ded0644c6462e2a38cafbb38f5af3abdddc2
johnedma <johned.matt@gmail.com>
2021-01-20 02:12:40 -0600

    test MobilecoindDirectory

---
+++ test/views/wallet/ConfigureMobilecoindView/MobilecoindDirectory.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import useMobilecoindConfigs from '../../../../app/hooks/useMobilecoindConfigs';
+import MobilecoindDirectory from '../../../../app/views/wallet/ConfigureMobilecoindView/MobilecoindDirectory';
+
+jest.mock('../../../../app/hooks/useMobilecoindConfigs');
+
+describe('MobilecoindDirectory', () => {
+  const mockUseMobilecoindConfigs = useMobilecoindConfigs as jest.MockedFunction<
+    typeof useMobilecoindConfigs
+  >;
+
+  // @ts-ignore mock
+  mockUseMobilecoindConfigs.mockImplementation(() => {
+    return {
+      ledgerDbPath: 'ledger/db/path',
+      mobilecoindDbPath: 'mobilecoin/db/path',
+    };
+  });
+  test('MobilecoindDirectory renders view and displays correct ledgerDbPath', () => {
+    render(<MobilecoindDirectory />);
+    expect(screen.getByText('ledger/db/path')).toBeInTheDocument();
+  });
+
+  test('MobilecoindDirectory renders view and displays correct mobilecoindDbPath', () => {
+    render(<MobilecoindDirectory />);
+    expect(screen.getByText('mobilecoin/db/path')).toBeInTheDocument();
+  });
+});

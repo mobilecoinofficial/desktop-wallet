8868b091e9030117696461bb1a0b5f308616742d
johnedma <johned.matt@gmail.com>
2021-01-25 13:09:48 -0600

    reviewing possbile guard conlfict w/o set entropy

--- test/components/WalletGuard/WalletGuard.test.tsx
+++ test/components/WalletGuard/WalletGuard.test.tsx
@@ -10,6 +10,7 @@ jest.mock('../../../app/hooks/useMobileCoinD');
 
 function setupComponent(contextOverides?: MobileCoinDContextValue) {
   const defaultContext = {
+    encryptedEntropy: null,
     isAuthenticated: false,
   };
 
@@ -28,11 +29,11 @@ function setupComponent(contextOverides?: MobileCoinDContextValue) {
   };
 }
 
-// -------------------
 describe('WalletGuard', () => {
   test('authenticated users are given access to children prop of WalletGuard', () => {
     // @ts-ignore mock
     const { success } = setupComponent({
+      encryptedEntropy: 'string',
       isAuthenticated: true,
     });
     expect(success).toBeInTheDocument();
@@ -41,6 +42,47 @@ describe('WalletGuard', () => {
   test('unauthenticated users are redirected to UnlockWalletView', () => {
     const { success } = setupComponent();
     expect(success).not.toBeInTheDocument();
-    expect(screen.queryByTestId('UnlockWalletView')).toBeInTheDocument();
+    // expect(screen.queryByTestId('DashboardOverview')).toBeInTheDocument(); ???
+    // expect(screen.queryByTestId('UnlockWalletView')).toBeInTheDocument(); ???
+    // IF ROUTEPATHS.ROOT GOES TO UNLOCKWALLETVIEW WHICH HAS UNLOCKWALLETGUARD
+    // THEN HAVING NO ENTROPY TAKES PRESCEDENCE OVER BEING UNAUTHENTICATED
+    // USER WILL BE REDIRECTED TO ROUTEPATHS.CREATE, NOT UNLOCKWALLETVIEW OR DASHBOARD OVERVIEW
+
+    // 2 DIFF ROUTEPATHS.ROOT ---> DASHBOARD AND UNLOCKWALLETVIEW
   });
 });
+
+// if you are authenticated you should always also have an encrypted entropy),
+// ------------
+
+
+// test('unauthenticated with no entropy string', () => {
+//   const { children } = setupComponent();
+
+//   expect(screen.queryByText('Create a new account for this desktop wallet.')).toBeInTheDocument();
+//   expect(children).not.toBeInTheDocument();
+// });
+
+// test('authenticated with no entropy string', () => {
+// // @ts-ignore mock
+//   const { children } = setupComponent({ encryptedEntropy: null, isAuthenticated: true });
+
+//   expect(screen.queryByTestId('DashboardOverview')).not.toBeInTheDocument();
+//   expect(children).not.toBeInTheDocument();
+// });
+
+// test('authenticated with entropy string', () => {
+// // @ts-ignore mock
+//   const { children } = setupComponent({ encryptedEntropy: 'entropy', isAuthenticated: true });
+
+//   expect(screen.queryByTestId('DashboardOverview')).toBeInTheDocument();
+//   expect(children).not.toBeInTheDocument();
+// });
+
+// test('unauthenticated with entropy string', () => {
+// // @ts-ignore mock
+//   const { children } = setupComponent({ encryptedEntropy: 'entropy' });
+
+//   expect(children).toBeInTheDocument();
+// });
+// });

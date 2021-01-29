54b35fb609e31f78e130c7cb3a34100abe02e395
johnedma <johned.matt@gmail.com>
2021-01-28 18:48:58 -0600

    MobilecoindDirectory added

--- app/views/wallet/ConfigureMobilecoindView/MobilecoindDirectory.tsx
+++ app/views/wallet/ConfigureMobilecoindView/MobilecoindDirectory.tsx
@@ -2,30 +2,32 @@ import React from 'react';
 import type { FC } from 'react';
 
 import { Box, FormLabel, Typography } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 import useMobilecoindConfigs from '../../../hooks/useMobilecoindConfigs';
 
 const MobilecoindDirectory: FC = () => {
   const { ledgerDbPath, mobilecoindDbPath } = useMobilecoindConfigs();
+  const { t } = useTranslation('MobilecoindDirectory');
 
   return (
     <Box flexGrow={1} mt={3}>
       <Box pt={3}>
         <FormLabel component="legend">
-          <Typography color="primary">MobileCoinD Directory</Typography>
+          <Typography color="primary">{t('formLabel')}</Typography>
         </FormLabel>
       </Box>
       <Box pt={2}>
         <Box py={1} />
         <Typography variant="body2" color="textPrimary">
-          Ledger Database Path:
+          {t('ledgerDbPathHeader')}
         </Typography>
         <Typography variant="body2" color="textSecondary">
           {`${ledgerDbPath}`}
         </Typography>
         <Box py={1} />
         <Typography variant="body2" color="textPrimary">
-          MobileCoinD Database Path:
+          {t('mobilecoindDbPathHeader')}
         </Typography>
         <Typography variant="body2" color="textSecondary">
           {`${mobilecoindDbPath}`}

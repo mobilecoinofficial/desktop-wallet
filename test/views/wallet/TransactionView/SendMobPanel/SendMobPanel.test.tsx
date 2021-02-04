8461c1b722d8580f34d9fbbc510776e8be291cce
johnedma <johned.matt@gmail.com>
2021-02-04 10:10:20 -0600

    Update SendMobPanel test

--- test/views/wallet/TransactionView/SendMobPanel/SendMobPanel.test.tsx
+++ test/views/wallet/TransactionView/SendMobPanel/SendMobPanel.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { screen } from '@testing-library/react';
 
-import SendMobPanel from '../../../../../app/views/wallet/TransactionView';
+import SendMobPanel from '../../../../../app/views/wallet/TransactionView/SendMobPanel';
 import renderSnapshot from '../../../../renderSnapshot';
 
 function setupComponent() {
@@ -10,15 +10,9 @@ function setupComponent() {
     <SendMobPanel />,
   );
 
-  const sendQuery = screen.queryByText(/Please enter the amount of MOB you want to send and the public address of the recipient./i);
-  const acctName = screen.queryByText('Account Name');
-  const acctBalance = screen.queryByText('Account Balance');
-  const recipientInput = screen.queryByText('Recipient Public Address');
+  const sendQuery = screen.queryByText('Please enter the amount of MOB you want to send and the public address of the recipient.');
 
   return {
-    acctBalance,
-    acctName,
-    recipientInput,
     sendQuery,
   };
 }
@@ -26,24 +20,10 @@ function setupComponent() {
 describe('SendMobPanel', () => {
   describe('component', () => {
     describe('render', () => {
-      test('SendMobPanel renders correctly with query text', () => {
+      test('SendMobPanel renders correctly with appropriate header', () => {
         const { sendQuery } = setupComponent();
         expect(sendQuery).toBeInTheDocument();
-      });
-
-      test('Account Name renders', () => {
-        const { acctName } = setupComponent();
-        expect(acctName).toBeInTheDocument();
-      });
-
-      test('Account Balance renders', () => {
-        const { acctBalance } = setupComponent();
-        expect(acctBalance).toBeInTheDocument();
-      });
-
-      test('Recipient input exists', () => {
-        const { recipientInput } = setupComponent();
-        expect(recipientInput).toBeInTheDocument();
+        expect(sendQuery).not.toBeNull();
       });
     });
   });

a227a24973d05138213431a209ad5b8e8e7eaf9c
David Y. Hall <me@davidyhall.com>
2021-02-05 21:00:16 -0800

    Change mobileocoin.com to mobilecoin.com (#33)

--- app/contexts/MobileCoinDContext.tsx
+++ app/contexts/MobileCoinDContext.tsx
@@ -278,7 +278,7 @@ const reducer = (state: MobileCoinDState, action: Action): MobileCoinDState => {
         balance,
         isAuthenticated: true,
         isEntropyKnown: true,
-        mobUrl: `https://mobileocoin.com/mob58/${b58Code}`,
+        mobUrl: `https://mobilecoin.com/mob58/${b58Code}`,
         monitorId,
         receiver,
       };
@@ -300,7 +300,7 @@ const reducer = (state: MobileCoinDState, action: Action): MobileCoinDState => {
         entropy,
         isAuthenticated: true,
         isEntropyKnown: false,
-        mobUrl: `https://mobileocoin.com/mob58/${b58Code}`,
+        mobUrl: `https://mobilecoin.com/mob58/${b58Code}`,
         monitorId,
         receiver,
       };
@@ -348,7 +348,7 @@ const reducer = (state: MobileCoinDState, action: Action): MobileCoinDState => {
         balance,
         isAuthenticated: true,
         isEntropyKnown: true,
-        mobUrl: `https://mobileocoin.com/mob58/${b58Code}`,
+        mobUrl: `https://mobilecoin.com/mob58/${b58Code}`,
         monitorId,
         receiver,
       };

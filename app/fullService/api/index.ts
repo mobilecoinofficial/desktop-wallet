b1928cc94f423a7c9ea593043333b7c4297faaf4
David Y. Hall <me@davidyhall.com>
2021-02-08 01:40:42 -0800

    Add api to index

--- app/fullService/api/index.ts
+++ app/fullService/api/index.ts
@@ -1,1 +1,15 @@
+export { default as changePassword } from './changePassword';
+export { default as createAddress } from './createAddress';
+export { default as getAccount } from './getAccount';
+export { default as getAllAccounts } from './getAllAccounts';
+export { default as getAllAddressesByAccount } from './getAllAddressesByAccount';
+export { default as getAllTransactionsByAccount } from './getAllTransactionsByAccount';
+export { default as getAllTxosByAccount } from './getAllTxosByAccount';
+export { default as getBalance } from './getBalance';
 export { default as getLockedStatus } from './getLockedStatus';
+export { default as getTransaction } from './getTransaction';
+export { default as getTxo } from './getTxo';
+export { default as importAccount } from './importAccount';
+export { default as setPassword } from './setPassword';
+export { default as unlock } from './unlock';
+export { default as updateAccountName } from './updateAccountName';

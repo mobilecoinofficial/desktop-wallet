d559aa92a0b0f09fc0a5b12fe9e16d574cbc3337
fktt22 <78449916+fktt22@users.noreply.github.com>
2021-02-20 19:12:08 -0300

    Include latest electron-updater (#42)
    * Include latest electron-updater
    
    * Added package-linux-for-updater to create AppImage
    
    * Automatic updater - tested with Linux
    
    Co-authored-by: Federico Kereki <f--.k--@--.com>

---
+++ app/utils/autoupdate.ts
@@ -0,0 +1,26 @@
+import { app } from 'electron';
+import { autoUpdater } from 'electron-updater';
+
+const myLog = (st: string) => {
+  console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+  console.log(st);
+  console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+};
+
+if (process.env.NODE_ENV === 'production') {
+  myLog('AUTOUPDATER ENABLED');
+
+  autoUpdater.on('checking-for-update', () => myLog('Checking for update...'));
+  autoUpdater.on('update-available', () => myLog('Update available.'));
+  autoUpdater.on('update-not-available', () => myLog('Update not available.'));
+  autoUpdater.on('error', (err) => myLog(`Error in auto-updater: ${err}`));
+  autoUpdater.on('download-progress', (pg) =>
+    myLog(`Downloaded ${pg.transferred}/${pg.total} ${pg.percent}%`)
+  );
+  autoUpdater.on('update-downloaded', () => myLog('Update downloaded'));
+
+  app.on('window-all-closed', () => app.quit());
+  app.on('ready', () => autoUpdater.checkForUpdatesAndNotify());
+} else {
+  myLog('AUTOUPDATER NOT ENABLED');
+}

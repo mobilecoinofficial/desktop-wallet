7751afa0d9c5782236c85eca95f242a94e5fc953
David Y. Hall <me@davidyhall.com>
2021-02-25 03:11:46 -0800

    Add useFullService to ReceiveMobPanel

--- app/views/wallet/TransactionView/ReceiveMobPanel.tsx
+++ app/views/wallet/TransactionView/ReceiveMobPanel.tsx
@@ -6,7 +6,7 @@ import {
 } from '@material-ui/core';
 
 import AccountCard from '../../../components/AccountCard';
-import useMobileCoinD from '../../../hooks/useMobileCoinD';
+import useFullService from '../../../hooks/useFullService';
 import type { Theme } from '../../../theme';
 
 const useStyles = makeStyles((theme: Theme) => {
@@ -25,12 +25,8 @@ const useStyles = makeStyles((theme: Theme) => {
 });
 
 const ReceiveMobPanel: FC = () => {
-  const { accountName, b58Code, mobUrl } = useMobileCoinD();
+  const { selectedAccount } = useFullService();
   const classes = useStyles();
-
-  // TODO - we should add a global modal request for bug reporting.
-  // Esp for unexpected states like this. (unless I am misundering mobilecoind)
-  if (b58Code === null || mobUrl === null) return <></>;
 
   return (
     <Container className={classes.cardContainer} maxWidth="sm">
@@ -51,7 +47,12 @@ const ReceiveMobPanel: FC = () => {
         </div>
       </Box>
       <Box display="flex" flexDirection="column" alignItems="center">
-        <AccountCard account={{ b58Code, mobUrl, name: accountName }} />
+        <AccountCard account={{
+          b58Code: selectedAccount.account.mainAddress,
+          mobUrl: selectedAccount.mobUrl,
+          name: selectedAccount.account.name,
+        }}
+        />
       </Box>
     </Container>
   );

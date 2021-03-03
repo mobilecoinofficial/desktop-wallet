c283a48addcbf51c64cf16069888b78361510adf
Brian <brian.william.corbin@gmail.com>
2021-03-03 16:35:00 -0500

    Fix for yarn start/package issue on Mac

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,28 +1,32 @@
 import i18n from 'i18next';
+// import i18nextBackend from 'i18next-node-fs-backend';
 import { initReactI18next } from 'react-i18next';
 
-import enUS from '../locales/enUS.json';
-
-const resources = {
-  enUS,
-};
+import en from '../locales/en/translation.json';
+import up from '../locales/up/translation.json'; // this is uppercased and for dev only
 
 i18n
   // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  .init({
+  // .use(i18nextBackend)
+  .use(initReactI18next);
+
+if (!i18n.isInitialized) {
+  i18n.init({
     debug: true,
-    fallbackLng: 'enUS',
+    fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    lng: 'enUS',
+    lng: 'en',
     react: {
       wait: false,
     },
-    resources,
-    supportedLngs: ['enUS'],
+    resources: {
+      en,
+      up,
+    },
+    supportedLngs: ['en', 'up'],
   });
+}
 
 export default i18n;
--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import React, { Fragment } from 'react';
 
+import { ipcRenderer } from 'electron';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 
+import i18n from './i18n';
+
 import './app.global.css';
-import './i18n';
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+
+ipcRenderer.sendSync('get-initial-translations');
+
+ipcRenderer.on('language-changed', (_, message) => {
+  if (!i18n.hasResourceBundle(message.language, message.namespace)) {
+    i18n.addResourceBundle(message.language, message.namespace, message.resource);
+  }
+
+  i18n.changeLanguage(message.language);
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line global-require
   const App = require('./App.tsx').default;
 
   render(
+    // <I18nextProvider i18n={ i18n } >
     <AppContainer>
+      {/* <I18nextProvider i18n={ i18n } initialI18nStore={ initialI18nStore } initialLanguage="el"> */}
       <App />
+      {/* </I18nextProvider> */}
     </AppContainer>,
+    // </I18nextProvider>,
     document.getElementById('root')
   );
 });

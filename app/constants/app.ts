d11d75a4a0227cb5e20c64cdaa6cce42fb2db0bd
Federico Kereki <f--.k--@--.com>
2021-03-12 21:17:09 -0300

    Detect inactivity

--- app/constants/app.ts
+++ app/constants/app.ts
@@ -2,3 +2,6 @@ export const APP_VERSION = process.env.npm_package_version;
 export const MIN_WINDOW_HEIGHT = 640;
 export const MIN_WINDOW_WIDTH = 720;
 export const INITIAL_WINDOW_HEIGHT = 880;
+
+export const TIME_FOR_INACTIVITY = 300000; // after this time, will be considered inactive: 5'
+export const TIME_FOR_REACTION = 30000; // extra time to avoid closing: 30"

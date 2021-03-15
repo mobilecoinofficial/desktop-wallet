fcffdaab5d8492af7551a3911c03b30b6e5d18b4
David Y. Hall <me@davidyhall.com>
2021-03-14 20:06:17 -0700

    Lint TransactionLog type

--- app/types/TransactionLog.ts
+++ app/types/TransactionLog.ts
@@ -17,7 +17,12 @@ export default interface TransactionLog {
   outputTxoIds: StringHex[];
   recipientAddressId: StringB58 | null;
   sentTime: string | null; // FIX-ME: Confirm type
-  status: 'tx_status_built' | 'tx_status_pending' | 'tx_status_received' | 'tx_status_succeded' | 'tx_status_failed';
+  status:
+    | 'tx_status_built'
+    | 'tx_status_pending'
+    | 'tx_status_received'
+    | 'tx_status_succeded'
+    | 'tx_status_failed';
   submittedBlockIndex: StringUInt64 | null;
   transactionLogId: StringHex;
   valuePmob: StringUInt64;

c87cce9031c845b513fcf46bf328cd8c26e5c42f
Federico Kereki <f--.k--@--.com>
2021-03-16 10:28:39 -0300

    Fix wrong type

--- app/types/WalletStatus.ts
+++ app/types/WalletStatus.ts
@@ -1,14 +1,14 @@
-import type Account from './FullServiceAccount';
+import type Account from './Account';
 import type { StringHex, StringUInt64 } from './SpecialStrings';
 
 export default interface WalletStatus {
   accountIds: StringHex[];
   accountMap: { [accountId: string]: Account };
-  isSyncedAll: boolean
+  isSyncedAll: boolean;
   localBlockIndex: StringUInt64;
   minSyncedBlockIndex: StringUInt64;
   networkBlockIndex: StringUInt64;
-  object: 'wallet_status'
+  object: 'wallet_status';
   totalOrphanedPmob: StringUInt64;
   totalPendingPmob: StringUInt64;
   totalSecretedPmob: StringUInt64;

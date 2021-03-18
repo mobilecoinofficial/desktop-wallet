1dad14b0b70952ea61c247c8343e18331e1373aa
John Jett Matthews <johned.matt@gmail.com>
2021-03-18 13:57:30 -0500

    imported nav svgs from figma (#70)

--- app/components/icons/CogIcon.tsx
+++ app/components/icons/CogIcon.tsx
@@ -6,18 +6,18 @@ function CogIcon(props: IconProps): JSX.Element {
   const { color } = props;
 
   return (
-    <svg viewBox="0 0 32 32" fill="none" {...props}>
+    <svg viewBox="0 0 28 28" fill="none" {...props}>
       <path
         d="M27 18v-4h-3.26a8.278 8.278 0 00-.85-2.06L24.83 10 22 7.17l-1.94 1.94A8.28 8.28 0 0018 8.26V5h-4v3.26a8.28 8.28 0 00-2.06.85L10 7.17 7.17 10l1.94 1.94A8.28 8.28 0 008.26 14H5v4h3.26a8.28 8.28 0 00.85 2.06L7.17 22 10 24.83l1.94-1.94c.645.375 1.338.66 2.06.85V27h4v-3.26a8.278 8.278 0 002.06-.85L22 24.83 24.83 22l-1.94-1.94c.375-.645.66-1.338.85-2.06H27z"
         stroke={color}
-        strokeWidth={2}
+        strokeWidth={1.7}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M16 19a3 3 0 100-6 3 3 0 000 6z"
         stroke={color}
-        strokeWidth={2}
+        strokeWidth={1.7}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -26,7 +26,7 @@ function CogIcon(props: IconProps): JSX.Element {
 }
 
 CogIcon.defaultProps = {
-  color: '#A6AAB4',
+  color: '#ADB0BB',
   height: 24,
   width: 24,
 };
--- app/components/icons/WalletHomeIcon.tsx
+++ app/components/icons/WalletHomeIcon.tsx
@@ -6,31 +6,25 @@ function WalletHomeIcon(props: IconProps): JSX.Element {
   const { color } = props;
 
   return (
-    <svg viewBox="0 0 32 32" fill="none" {...props}>
-      <g
-        clipPath="url(#prefix__clip0)"
-        stroke={color}
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M26 10H6a2 2 0 00-2 2v14a2 2 0 002 2h20a2 2 0 002-2V12a2 2 0 00-2-2z" />
-        <path d="M28 22h-3a3 3 0 010-6h3M6 10l5.29-5.29a1 1 0 011.42 0L18 10" />
-        <path d="M15 7l3.36-2.52a1 1 0 011.25 0L26 10" />
-      </g>
-      <defs>
-        <clipPath id="prefix__clip0">
-          <path fill="#fff" d="M0 0h32v32H0z" />
-        </clipPath>
-      </defs>
+    <svg
+      // width={27}
+      // height={27}
+      viewBox="0 0 27 27"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M25.35 12.258l-.002-.002L14.742 1.65a2.377 2.377 0 00-1.692-.7c-.64 0-1.24.248-1.693.7l-10.6 10.6-.011.012a2.396 2.396 0 00.004 3.38 2.38 2.38 0 001.663.701h.422v7.805a2.805 2.805 0 002.802 2.801h4.149c.42 0 .762-.34.762-.762v-6.119a1.28 1.28 0 011.278-1.278h2.447a1.28 1.28 0 011.278 1.278v6.12c0 .42.341.761.762.761h4.15a2.805 2.805 0 002.801-2.801v-7.805h.392c.64 0 1.24-.249 1.693-.701.932-.933.932-2.45.001-3.384zm-1.079 2.307a.865.865 0 01-.615.255h-1.154a.762.762 0 00-.761.761v8.567a1.28 1.28 0 01-1.279 1.278h-3.387v-5.358a2.805 2.805 0 00-2.802-2.801h-2.447a2.805 2.805 0 00-2.802 2.801v5.358H5.637a1.28 1.28 0 01-1.279-1.278v-8.567a.762.762 0 00-.761-.761H2.463a.864.864 0 01-.635-.255.871.871 0 010-1.231l.001-.001L12.434 2.728a.864.864 0 01.616-.255c.232 0 .45.09.615.255L24.268 13.33l.005.005c.337.34.337.891-.002 1.23z"
+        fill={color}
+      />
     </svg>
   );
 }
 
 WalletHomeIcon.defaultProps = {
-  color: '#A6AAB4',
-  height: 32,
-  width: 32,
+  color: '#ADB0BB',
+  height: 27,
+  width: 27,
 };
 
 export default WalletHomeIcon;

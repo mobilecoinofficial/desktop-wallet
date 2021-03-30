6890861bffe7634853d1240311887339504d08f6
David Y. Hall <me@davidyhall.com>
2021-03-30 00:42:19 -0700

    Make dropdown select address

--- app/views/wallet/TransactionView/ReceiveMobPanel.tsx
+++ app/views/wallet/TransactionView/ReceiveMobPanel.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { FC } from 'react';
 
-import { Box, Container, makeStyles, Typography } from '@material-ui/core';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Container,
+  MenuItem,
+  Select,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
+import { useTranslation } from 'react-i18next';
 
 import AccountCard from '../../../components/AccountCard';
 import useFullService from '../../../hooks/useFullService';
 import type { Theme } from '../../../theme';
+import * as localStore from '../../../utils/LocalStore';
 
 const useStyles = makeStyles((theme: Theme) => ({
   cardContainer: {
@@ -20,29 +33,76 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
+// CBB, really, we should just give the list and work by the indexes.
 const ReceiveMobPanel: FC = () => {
   const { selectedAccount } = useFullService();
+  const { mainAddress } = selectedAccount.account;
+  const [selectedAddress, setSelectedAddress] = useState(mainAddress);
   const classes = useStyles();
+  const { t } = useTranslation('ReceiveMobPanel');
+  const listOfContacts = localStore.getContacts();
+  const nameFromAddress =
+    selectedAddress === mainAddress
+      ? 'Main Public Address'
+      : listOfContacts.find((contact) => contact.assignedAddress === selectedAddress)?.alias;
+
+  const dropDownValues = [{ alias: 'Main Public Address', assignedAddress: mainAddress }].concat(
+    listOfContacts
+  );
 
   return (
     <Container className={classes.cardContainer} maxWidth="sm">
-      <Box alignItems="center" display="flex" justifyContent="space-between" mb={3}>
-        <div>
-          <Typography variant="body2" color="textSecondary">
-            To receive MOB, you must share your public address code to the sender. The code is the
-            text you see on your account card. Simply click the code to copy it to your clipboard.
-            Alternatively, the sender can scan your account QR code. Press the QR code icon to
-            switch views.
-          </Typography>
-        </div>
-      </Box>
+      <Select
+        style={{ width: '100%' }}
+        labelId="contactsList"
+        id="contactsList"
+        value={selectedAddress}
+        displayEmpty
+        onChange={(x) => {
+          setSelectedAddress(x.target.value);
+        }}
+      >
+        {dropDownValues.map((contact) => (
+          <MenuItem value={contact.assignedAddress} key={contact.assignedAddress}>
+            {contact.alias}
+          </MenuItem>
+        ))}
+      </Select>
+
       <Box display="flex" flexDirection="column" alignItems="center">
         <AccountCard
           account={{
-            b58Code: selectedAccount.account.mainAddress,
-            name: selectedAccount.account.name,
+            b58Code: selectedAddress,
+            name: nameFromAddress,
           }}
         />
+      </Box>
+
+      <Box mt={3}>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>{t('howTo')}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box>
+              <Typography variant="body2">{t('howToReceiveDetails')}</Typography>
+              <Box paddingY={1} />
+              <Typography variant="body2" color="primary">
+                {t('mainPublicAddress')}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                {t('mainPublicAddressDetails')}
+              </Typography>
+              <Box paddingY={1} />
+              <Typography variant="body2" color="primary">
+                {t('assignedAddress')}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                {t('assignedAddressDetails')}
+              </Typography>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
       </Box>
     </Container>
   );

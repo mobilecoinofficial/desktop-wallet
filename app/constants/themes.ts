a463cf585deaddda63ac61320bd189e7d6a5f9a9
David Y. Hall <me@davidyhall.com>
2021-03-31 03:21:48 -0700

    Change dark theme

--- app/constants/themes.ts
+++ app/constants/themes.ts
@@ -1,20 +1,33 @@
 import {
   BLACK,
+  BLACK_CHARADE,
   BLACK_DARK,
+  BLACK_SHARK,
+  BLUE_DODGER,
+  BLUE_NEON_BLUE,
   GOLD,
   GOLD_DARK,
   GOLD_LIGHT,
+  GREEN_EMERALD,
+  GREEN_PUERTO_RICO,
   GREY,
   GREY_DARK,
   GREY_LIGHT,
+  GREY_MERCURY,
+  ORANGE_NEON_CARROT,
+  ORANGE_ORANGE,
+  PINK_LAVENDER_ROSE,
+  PURPLE_BLUE_MOODY,
   RED,
+  RED_BITTERSWEET,
   WHITE,
+  WHITE_IVORY,
 } from './colors';
 
 export const MOBILE_COIN_DARK = 'MOBILE_COIN_DARK';
 export const MOBILE_COIN_LIGHT = 'MOBILE_COIN_LIGHT';
 
-export const MOBILE_COIN_DARK_THEME = {
+export const MOBILE_COIN_DARK_GOLD_THEME = {
   name: MOBILE_COIN_DARK,
   palette: {
     background: {
@@ -41,6 +54,38 @@ export const MOBILE_COIN_DARK_THEME = {
       primary: GREY_LIGHT,
       secondary: GREY,
       tertiary: GOLD,
+    },
+    type: 'dark',
+  },
+};
+
+export const MOBILE_COIN_DARK_THEME = {
+  name: MOBILE_COIN_DARK,
+  palette: {
+    background: {
+      dark: BLACK_SHARK,
+      default: BLACK_CHARADE,
+      paper: BLACK_CHARADE,
+    },
+    longCode: {
+      lowercased: PINK_LAVENDER_ROSE,
+      number: ORANGE_NEON_CARROT,
+      uppercased: BLUE_DODGER,
+    },
+    number: {
+      negative: RED_BITTERSWEET,
+      positive: GREEN_PUERTO_RICO,
+    },
+    primary: {
+      main: BLUE_NEON_BLUE,
+    },
+    secondary: {
+      main: PURPLE_BLUE_MOODY,
+    },
+    text: {
+      primary: GREY_MERCURY,
+      secondary: WHITE_IVORY,
+      tertiary: BLUE_NEON_BLUE,
     },
     type: 'dark',
   },

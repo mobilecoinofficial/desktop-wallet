fa1cc23aea4f5b9fe6a00b37f049de15c488a903
Federico Kereki <f--.k--@--.com>
2021-04-02 20:35:29 -0300

    Link checkbox to Formik

--- app/components/StarCheckbox.tsx
+++ app/components/StarCheckbox.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import type { FC } from 'react';
 
 import { Checkbox } from '@material-ui/core';
 
 import { StarIcon, StarOutlineIcon } from './icons';
 
-const StarCheckbox = (...props) => (
-  <Checkbox checkedIcon={<StarIcon />} icon={<StarOutlineIcon />} {...props} />
+interface StarCheckboxProps {
+  formik?: unknown;
+}
+
+const StarCheckbox: FC<StarCheckboxProps> = (formik) => (
+  <Checkbox
+    checkedIcon={<StarIcon />}
+    icon={<StarOutlineIcon />}
+    {...formik?.field}
+    {...formik?.props}
+  />
 );
+
+StarCheckbox.defaultProps = {
+  formik: {},
+};
 
 export default StarCheckbox;

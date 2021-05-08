6d1d6228ed9eda38b518ac92b9c1b1dcd149a55a
John Jett Matthews <johned.matt@gmail.com>
2021-05-07 20:46:49 -0500

    Clean up for code in previous components PR (#130)

--- app/components/TermsOfUseDialog/TermsOfUseDialog.test.tsx
+++ app/components/TermsOfUseDialog/TermsOfUseDialog.test.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable jest/no-commented-out-tests */
 import React from 'react';
 
-import 'jest-canvas-mock';
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
@@ -11,11 +9,21 @@ import '../../testUtils/i18nForTests';
 
 const handleCloseTerms = jest.fn();
 describe('TermsOfUseDialog', () => {
-  test('renders long code with tooltip by default and toggles correctly', () => {
+  test('renders TermsOfUse and closes on click', () => {
     render(<TermsOfUseDialog open handleCloseTerms={handleCloseTerms} />);
+
+    expect(screen.getByTestId('tos-header').textContent).toEqual(
+      'MOBILECOIN TERMS OF USE FOR MOBILECOINS AND MOBILECOIN WALLETS'
+    );
 
     userEvent.click(screen.getByText('Close Terms of Use'));
 
     expect(handleCloseTerms).toHaveBeenCalled();
+  });
+
+  test('TermsOfUse not rendered if open prop is false', () => {
+    render(<TermsOfUseDialog open={false} handleCloseTerms={handleCloseTerms} />);
+
+    expect(screen.queryByTestId('tos-header')).toBeNull();
   });
 });

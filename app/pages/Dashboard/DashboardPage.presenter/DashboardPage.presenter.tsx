42eda13557c6d4cc9fcdb5f0239e24bd5dc7b85d
Federico Kereki <f--.k--@--.com>
2021-05-11 20:26:48 -0300

    [MOBDW-267] Avoid using ipcRenderer in pages

--- app/pages/Dashboard/DashboardPage.presenter/DashboardPage.presenter.tsx
+++ app/pages/Dashboard/DashboardPage.presenter/DashboardPage.presenter.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import type { FC } from 'react';
 
 import { Box, Container, Grid, makeStyles } from '@material-ui/core';
-import { ipcRenderer } from 'electron';
 
 import useFullService from '../../../hooks/useFullService';
 import type { Theme } from '../../../theme';
 import { DashboardView } from '../DashboardPage.view/DashboardPage.view';
+import type { DashboardPageProps } from './DashboardPage.d';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -17,17 +17,16 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-const DashboardPage: FC = () => {
+const DashboardPage: FC<DashboardPageProps> = ({ onClose }: DashboardPageProps) => {
   const classes = useStyles();
   const { selectedAccount } = useFullService();
-  const handleCloseApp = () => ipcRenderer.send('close-app');
 
   return (
     <Box data-testid="DashboardPage" className={classes.root}>
       <Container maxWidth={false}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <DashboardView selectedAccount={selectedAccount} onClose={handleCloseApp} />
+            <DashboardView selectedAccount={selectedAccount} onClose={onClose} />
           </Grid>
         </Grid>
       </Container>

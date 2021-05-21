eb47e408e23125b64ce1e88c38fe2bac166380e2
Federico Kereki <f--.k--@--.com>
2021-05-21 19:45:20 -0300

    More tests at 100% coverage

--- app/pages/Settings/SettingsOptionsItem.view/SettingsOptionsItem.test.tsx
+++ app/pages/Settings/SettingsOptionsItem.view/SettingsOptionsItem.test.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import '../../../testUtils/i18nForTests';
+import userEvent from '@testing-library/user-event';
 
 import { LockIcon } from '../../../components/icons';
 import { SettingsOptionsItem } from './SettingsOptionsItem.view';
 
 describe('SettingsOptionsItem', () => {
+  const handleOnClick = jest.fn();
+  const CHANGE_PASSWORD = 'changePassword';
+
   test('Displays correct option item', () => {
-    const handleOnClick = jest.fn();
-    const CHANGE_PASSWORD = 'changePassword';
     const { getByText } = render(
       <SettingsOptionsItem
         Icon={LockIcon}
@@ -21,5 +23,20 @@ describe('SettingsOptionsItem', () => {
     );
 
     expect(getByText('Change Passphrase')).toBeInTheDocument();
+  });
+
+  test('Click works', () => {
+    const { container } = render(
+      <SettingsOptionsItem
+        Icon={LockIcon}
+        label="changePassword"
+        path={CHANGE_PASSWORD}
+        handleOnClick={handleOnClick}
+      />
+    );
+
+    const card = container.querySelector('[name="card-action"]') as HTMLInputElement;
+    userEvent.click(card);
+    expect(handleOnClick).toHaveBeenCalled();
   });
 });

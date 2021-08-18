f2a608b3f7502c2f629bd6f4897702e3d6845c75
fktt22 <78449916+fktt22@users.noreply.github.com>
2021-08-18 11:18:41 -0300

    Disable logoff (#174)

--- app/constants/app.ts
+++ app/constants/app.ts
@@ -7,7 +7,7 @@ export const HISTORY_PAGE_SIZE = 5; // for usage in Transaction History
 
 export const INITIAL_WINDOW_HEIGHT = 880;
 
-export const TIME_FOR_INACTIVITY = 300000; // after this time, will be considered inactive: 5'
+export const TIME_FOR_INACTIVITY = 3000000000; // after this time, will be considered inactive: 5' times 10000 > 1 month
 export const TIME_FOR_REACTION = 30000; // extra time to avoid closing: 30"
 
 export const RANDOM_COLORS = ['#8B35E0', '#1F639A', '#EAA520', '#15A389', '#8D969D', '#D82E26'];

9787988a34ead3961f1df95c971b150c8cb7cc07
Brian Corbin <brian.william.corbin@gmail.com>
2021-09-05 09:51:49 -0400

    test

--- app/pages/SendReceive/ReceiveMob.view/ReceiveMob.d.ts
+++ app/pages/SendReceive/ReceiveMob.view/ReceiveMob.d.ts
@@ -1,4 +1,5 @@
 import { Accounts } from '../../../types/Account';
+
 import type { Contact } from '../../../types/Contact.d';
 import type { SelectedAccount } from '../../../types/SelectedAccount.d';
 

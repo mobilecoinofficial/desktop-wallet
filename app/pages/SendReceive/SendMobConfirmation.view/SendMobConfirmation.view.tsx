b28f9635e96c165ea4cab8ce6ff65dfd60546d10
Federico Kereki <federico.kereki@toptal.com>
2021-09-10 21:22:30 -0300

    Created needed files

---
+++ app/pages/SendReceive/SendMobConfirmation.view/SendMobConfirmation.view.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { FC } from 'react';
+
+import type { SendMobConfirmationProps } from './SendMobConfirmation.d';
+
+const SendMobConfirmation: FC<SendMobConfirmationProps> = () => <div>xxx</div>;
+
+export default SendMobConfirmation;
+export { SendMobConfirmation };

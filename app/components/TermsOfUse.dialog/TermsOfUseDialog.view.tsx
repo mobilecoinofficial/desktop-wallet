5a5e05138176182530f25e1b1e0b8444cdf98754
John Jett Matthews <johned.matt@gmail.com>
2022-01-06 13:00:10 -0600

    FAB added to terms of use (#225)
    * FAB added to terms of use
    
    * FAB colors inverted and hover animation added

--- app/components/TermsOfUse.dialog/TermsOfUseDialog.view.tsx
+++ app/components/TermsOfUse.dialog/TermsOfUseDialog.view.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 
-import { Box, Button, Container, Dialog } from '@material-ui/core';
+import { Box, Button, Container, Dialog, Fab } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import { useTranslation } from 'react-i18next';
 
 import { TermsOfUse } from '../TermsOfUse.component';
 import { TermsOfUseDialogProps } from './TermsOfUseDialog';
 
+const useStyles = makeStyles({
+  fab: {
+    '&:active': {
+      boxShadow:
+        '0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);',
+    },
+    '&:hover .MuiSvgIcon-root': {
+      fontSize: '3.5em',
+      transition: '0.1s ease-in-out',
+    },
+    border: '4px solid #00000014',
+    borderRadius: '1em',
+    bottom: 102,
+    cursor: 'default',
+    height: '4em',
+    position: 'fixed',
+    right: 42,
+  },
+});
+
 const TermsOfUseDialog = (props: TermsOfUseDialogProps): JSX.Element => {
   const { open, handleCloseTerms } = props;
   const { t } = useTranslation('TermsOfUseDialog');
+  const classes = useStyles();
 
   return (
     <Dialog
@@ -22,6 +45,9 @@ const TermsOfUseDialog = (props: TermsOfUseDialogProps): JSX.Element => {
     >
       <Container maxWidth="md">
         <TermsOfUse />
+        <Fab disableRipple aria-label="Scroll Down" className={classes.fab} color="primary">
+          <ArrowDownwardIcon color="inherit" fontSize="large" />
+        </Fab>
         <Box p={2}>
           <Button
             color="secondary"

c9974246a98a9cc3764b9d318c9b13dcfa9833ae
Davey Alvarez <daveyalvarez03@gmail.com>
2022-03-07 10:59:36 -0800

    pass versioning, menu title

--- app/menus/otherMenu.js
+++ app/menus/otherMenu.js
@@ -1,15 +1,16 @@
 import { shell, nativeTheme, BrowserWindow } from 'electron';
 
 import config from '../../configs/app.config';
+import { version } from '../../package.json';
 import * as localStore from '../utils/LocalStore';
 
-const openAboutWindow = async (mainWindow) => {
+const openAboutWindow = async (mainWindow, i18n) => {
   const modal = new BrowserWindow({
     autoHideMenuBar: true,
     height: 150,
     modal: true,
     parent: mainWindow,
-    title: __dirname,
+    title: i18n.t('Menu.about'),
     webPreferences: {
       contextIsolation: true,
       disableBlinkFeatures: 'Auxclick',
@@ -18,21 +19,18 @@ const openAboutWindow = async (mainWindow) => {
     width: 300,
   });
 
-  // await modal.loadFile('../../app/menus/otherHelp.html');
-  await modal.loadFile('app/menus/otherHelp.html', {
-    query: { version: process.env.npm_package_version },
+  await modal.loadFile('../../app/menus/otherHelp.html', {
+    query: { version },
   });
   return modal;
 };
-
-// /tmp/.mount_MobileKZdlrL/resources/app.asar
 
 const defaultTemplate = (app, mainWindow, i18n) => {
   const submenuAbout = {
     label: i18n.t('Menu.mobileCoin'),
     submenu: [
       {
-        click: () => openAboutWindow(mainWindow),
+        click: () => openAboutWindow(mainWindow, i18n),
         label: i18n.t('Menu.about'),
         selector: 'orderFrontStandardAboutPanel:',
       },

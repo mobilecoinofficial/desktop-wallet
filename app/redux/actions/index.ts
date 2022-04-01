c9327193d762cd5b0a373f970d38e2e3c73239d7
David Alvarez <daveyalvarez03@gmail.com>
2022-04-01 14:43:24 -0700

    rename actions

--- app/redux/actions/index.ts
+++ app/redux/actions/index.ts
@@ -1,37 +1,45 @@
-import { addAccountAction, AddAccountAction, ADD_ACCOUNT } from './addAccount';
+import { addAccountAction, AddAccountAction, ADD_ACCOUNT } from './addAccountAction';
 import {
   confirmEntropyKnownAction,
   CONFIRM_ENTROPY_KNOWN,
   ConfirmEntropyKnownAction,
-} from './confirmEntropyKnown';
-import { createAccountAction, CREATE_ACCOUNT, CreateAccountAction } from './createAccount';
-import { createWalletAction, CREATE_WALLET, CreateWalletAction } from './createWallet';
-import { deleteAccountAction, DELETE_ACCOUNT, DeleteAccountAction } from './deleteAccount';
-import { deleteWalletAction, DELETE_WALLET, DeleteWalletAction } from './deleteWallet';
+} from './confirmEntropyKnownAction';
+import { createAccountAction, CREATE_ACCOUNT, CreateAccountAction } from './createAccountAction';
+import { createWalletAction, CREATE_WALLET, CreateWalletAction } from './createWalletAction';
+import { deleteAccountAction, DELETE_ACCOUNT, DeleteAccountAction } from './deleteAccountAction';
+import { deleteWalletAction, DELETE_WALLET, DeleteWalletAction } from './deleteWalletAction';
 import {
   getAllGiftCodesAction,
   GET_ALL_GIFT_CODES,
   GetAllGiftCodesAction,
-} from './getAllGiftCodes';
+} from './getAllGiftCodesAction';
 import {
   getAllTransactionLogsForAccountAction,
   GET_ALL_TRANSACTION_LOGS_FOR_ACCOUNT,
   GetAllTransactionLogsForAccountAction,
-} from './getAllTransactionLogsForAccount';
+} from './getAllTransactionLogsForAccountAction';
 import {
   getAllTxosForAccountAction,
   GET_ALL_TXOS_FOR_ACCOUNT,
   GetAllTxosForAccountAction,
-} from './getAllTxosForAccount';
-import { getFeePmobAction, GetFeePmobAction, GET_FEE_PMOB } from './getFeePmob';
-import { importAccountAction, IMPORT_ACCOUNT, ImportAccountAction } from './importAccount';
-import { initializeAction, INITIALIZE, InitializeAction } from './initialize';
-import { selectAccountAction, SELECT_ACCOUNT, SelectAccountAction } from './selectAccount';
-import { unlockWalletAction, UNLOCK_WALLET, UnlockWalletAction } from './unlockWallet';
-import { updateContactsAction, UPDATE_CONTACTS, UpdateContactsAction } from './updateContacts';
-import { updatePasswordAction, UPDATE_PASSWORD, UpdatePasswordAction } from './updatePassword';
-import { updatePinAction, UPDATE_PIN, UpdatePinAction } from './updatePin';
-import { updateStatusAction, UPDATE_WALLET_STATUS, UpdateStatusAction } from './updateStatus';
+} from './getAllTxosForAccountAction';
+import { getFeePmobAction, GetFeePmobAction, GET_FEE_PMOB } from './getFeePmobAction';
+import { importAccountAction, IMPORT_ACCOUNT, ImportAccountAction } from './importAccountAction';
+import { initializeAction, INITIALIZE, InitializeAction } from './initializeAction';
+import { selectAccountAction, SELECT_ACCOUNT, SelectAccountAction } from './selectAccountAction';
+import { unlockWalletAction, UNLOCK_WALLET, UnlockWalletAction } from './unlockWalletAction';
+import {
+  updateContactsAction,
+  UPDATE_CONTACTS,
+  UpdateContactsAction,
+} from './updateContactsAction';
+import {
+  updatePasswordAction,
+  UPDATE_PASSWORD,
+  UpdatePasswordAction,
+} from './updatePasswordAction';
+import { updatePinAction, UPDATE_PIN, UpdatePinAction } from './updatePinAction';
+import { updateStatusAction, UPDATE_WALLET_STATUS, UpdateStatusAction } from './updateStatusAction';
 
 // Redux Types
 export {

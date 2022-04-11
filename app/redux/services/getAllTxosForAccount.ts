8bb78ecf7e9d58f5204cb6f83f6d4dd2d1859719
David Alvarez <daveyalvarez03@gmail.com>
2022-04-11 13:23:51 -0700

    fix tripleeq

--- app/redux/services/getAllTxosForAccount.ts
+++ app/redux/services/getAllTxosForAccount.ts
@@ -1,6 +1,7 @@
 import * as fullServiceApi from '../../fullService/api';
 import { StringHex } from '../../types';
 import { errorToString } from '../../utils/errorHandler';
+import sameObject from '../../utils/sameObject';
 import { getAllTxosForAccountAction } from '../actions';
 import { store } from '../store';
 
@@ -8,7 +9,7 @@ export const getAllTxosForAccount = async (accountId: StringHex): Promise<void> 
   try {
     const txos = await fullServiceApi.getAllTxosForAccount({ accountId });
 
-    if (store.getState().txos !== txos) {
+    if (!sameObject(store.getState().txos, txos)) {
       store.dispatch(getAllTxosForAccountAction(txos));
     }
   } catch (err) {

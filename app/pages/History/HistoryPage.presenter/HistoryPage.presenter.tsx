00cf7f2b92105527a6982049bde4df036dad5e9b
Colin Carey <colin.carey1@gmail.com>
2022-09-13 16:17:20 -0700

    Fix Confirmations in new version (#256)
    * Fix confirmations and txo detail display
    
    * Handle txoId input just in case

--- app/pages/History/HistoryPage.presenter/HistoryPage.presenter.tsx
+++ app/pages/History/HistoryPage.presenter/HistoryPage.presenter.tsx
@@ -64,7 +64,9 @@ export const HistoryPage: FC = (): JSX.Element => {
         const result = await getConfirmations({
           transactionLogId: currentTransactionLog.transactionLogId,
         });
-        clipboard.writeText(JSON.stringify(result.confirmations));
+        // newer versions of full service use txoId instead of txoIdHex
+        // replace newer field with older to maintain backwards compatibility
+        clipboard.writeText(JSON.stringify(result.confirmations).replace(/txoId/, 'txoIdHex'));
         enqueueSnackbar('Copied Confirmations to Clipboard');
       } catch (err) {
         const errorMessage = errorToString(err);
@@ -77,9 +79,12 @@ export const HistoryPage: FC = (): JSX.Element => {
     (async () => {
       const confirmationsString = clipboard.readText();
       try {
-        const confirmations = JSON.parse(confirmationsString) as Confirmations;
+        const confirmations = JSON.parse(
+          confirmationsString.replace(/"txoId"/, '"txoIdHex"')
+        ) as Confirmations;
         const results: { [txoId: string]: boolean } = {};
-
+        // newer versions of full service use txoId instead of txoIdHex
+        // replace newer field with older to maintain backwards compatibility
         await Promise.all(
           confirmations.map(async (confirmation) => {
             try {

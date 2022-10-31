f0e2371aff720ef1c0fb0ec56770401df15d0a3c
Colin Carey <colin.carey1@gmail.com>
2022-10-31 11:39:35 -0700

    increase app timeout to 10s (#275)

--- app/fullService/axiosFullService.ts
+++ app/fullService/axiosFullService.ts
@@ -52,7 +52,7 @@ const axiosFullService = async <T>(
         method,
         params: snakeCaseParams,
       },
-      timeout: 3500,
+      timeout: 10000,
     });
     // @ts-ignore override
     if (!response.jsonrpc) {

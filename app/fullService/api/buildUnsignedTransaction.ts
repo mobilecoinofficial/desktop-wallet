aed5fa189da76cf2507f7d28c3c40afface008f3
Colin Carey <colin.carey1@gmail.com>
2022-11-23 10:55:30 -0800

    Increase tombstone block for transaction builds in offline and view-only (#285)
    * Add tombstone logic
    
    * Use mx tombstone from lib

--- app/fullService/api/buildUnsignedTransaction.ts
+++ app/fullService/api/buildUnsignedTransaction.ts
@@ -1,5 +1,5 @@
 import type { StringHex, StringB58, StringUInt64 } from '../../types/SpecialStrings.d';
-import type { AddressAndAmount } from '../../types/TransactionLog';
+import type { AddressAndAmount } from '../../types/TransactionAmount';
 import type { UnsignedTxProposal } from '../../types/TxProposal';
 import axiosFullService, { AxiosFullServiceResponse } from '../axiosFullService';
 
@@ -9,6 +9,7 @@ export type BuildUnsignedTransactionParams = {
   accountId: StringHex;
   addressesAndAmounts: AddressAndAmount[];
   feeValue: StringUInt64;
+  tombstoneBlock: StringUInt64;
 };
 
 export type BuildUnsignedTransactionResult = {
@@ -25,12 +26,14 @@ const buildUnsignedTransaction = async ({
   addressesAndAmounts,
   accountId,
   feeValue,
+  tombstoneBlock,
 }: BuildUnsignedTransactionParams): Promise<BuildUnsignedTransactionResult> => {
   const { result, error }: AxiosFullServiceResponse<BuilTransactionProposalResponse> =
     await axiosFullService(BUILD_UNSIGNED_TRANSACTION_METHOD, {
       accountId,
       addressesAndAmounts,
       feeValue,
+      tombstoneBlock,
     });
 
   if (error) {

632ccec9629e77fc43c3d04aec0e48b4ee1821c7
Colin Carey <colin.carey1@gmail.com>
2022-11-28 10:06:46 -0800

    remove api key from command to start FS

--- full-service-bin/testnet/start-full-service.sh
+++ full-service-bin/testnet/start-full-service.sh
@@ -16,14 +16,11 @@ killall full-service || true
 LEDGER_DB_DIR="$1"
 WALLET_DB_DIR="$2"
 WALLET_DB_FILE="$3"
-MC_API_KEY="$4"
 
 mkdir -p "${LEDGER_DB_DIR}"
 mkdir -p "${WALLET_DB_DIR}"
 
-echo "Starting full-service with ${LEDGER_DB_DIR} and ${WALLET_DB_DIR} and ${WALLET_DB_FILE} and api-key ${MC_API_KEY}" > /tmp/mylog
-
-export MC_API_KEY=${MC_API_KEY}
+echo "Starting full-service with ${LEDGER_DB_DIR} and ${WALLET_DB_DIR} and ${WALLET_DB_FILE}"
 
 RUST_LOG=debug,mc_connection=error,mc_ledger_sync=error ./full-service \
         --ledger-db "${LEDGER_DB_DIR}" \

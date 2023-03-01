8ed166251a71e840a3ead9ce57bd202dcebe8095
Brian <brian.william.corbin@gmail.com>
2023-02-28 16:46:02 -0800

    disabling app updater

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -313,7 +313,7 @@ const createWindow = async () => {
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
-  new AppUpdater();
+  // new AppUpdater();
 };
 
 /**

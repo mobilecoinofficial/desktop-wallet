456ec57e64518a184c3bef153db9ea9f05f8ddb3
Henry Holtzman <henry@mobilecoin.com>
2024-11-27 16:16:31 -0800

    update tests

--- app/pages/History/HistoryList.view/HistoryList.d.ts
+++ app/pages/History/HistoryList.view/HistoryList.d.ts
@@ -3,4 +3,8 @@ import type { TransactionLog } from '../../../types/TransactionLog.d';
 export interface HistoryListProps {
   onTransactionClick: (transactionLog: TransactionLog) => void;
   transactionLogsList: TransactionLog[];
+  firstToShow: number;
+  setFirstToShow: (value: number) => void;
+  selectedTabIndex: number;
+  setSelectedTabIndex: (value: number) => void;
 }
